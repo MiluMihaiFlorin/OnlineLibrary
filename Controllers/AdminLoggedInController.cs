0a96c9fb90d54e81b211b2acbc78c5021ec18d34
MiluMihaiFlorin <milu.mihai.y3c@student.ucv.ro>
2023-03-12 23:01:17 +0200

    Second commit

---
+++ Controllers/AdminLoggedInController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineLibrary.Controllers
+{
+	public class AdminLoggedInController : Controller
+	{
+		private readonly ILogger<AdminLoggedInController> _logger;
+
+		public AdminLoggedInController(ILogger<AdminLoggedInController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Dashbord()
+		{
+			return View();
+		}
+
+        public IActionResult Categories()
+        {
+            return View();
+        }
+
+        public IActionResult Authors()
+        {
+            return View();
+        }
+        public IActionResult Books()
+        {
+            return View();
+        }
+        public IActionResult IssueBooks()
+        {
+            return View();
+        }
+        public IActionResult RegisterStudents()
+        {
+            return View();
+        }
+
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,12 +18,26 @@ namespace OnlineLibrary.Controllers
             return View();
         }
 
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
